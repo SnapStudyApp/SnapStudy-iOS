e6a0aaaebcefd7122de6b2ff8100ffafccaa7c5e
jomillsjoseanand <44730936+jomillsjoseanand@users.noreply.github.com>
2022-05-15 19:31:31 -0400

    Create SwiftUI project files and set up environment
    Created a SwiftUI app and set up file system to work on
    iOS application. Furthermore, got Git set up for version
    controlling the progress of the app.

---
+++ SnapStudy/SnapStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapStudyApp.swift
+//  SnapStudy
+//
+//  Created by Jomills Jose Anand on 2022-05-15.
+//
+
+import SwiftUI
+
+@main
+struct SnapStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
