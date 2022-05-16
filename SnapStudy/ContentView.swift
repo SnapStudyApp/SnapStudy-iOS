7c462c9d73b7dfe939d0adeaec803df672a7d488
jomillsjoseanand <44730936+jomillsjoseanand@users.noreply.github.com>
2022-05-16 19:14:43 -0400

    Develop image upload feature
    Created icons for opening camera and for choosing
    to upload pictures. The upload icon is functional.

--- SnapStudy/ContentView.swift
+++ SnapStudy/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+  @State var isPresenting: Bool = false
+  @State var uiImage: UIImage?
+      
+  var body: some View {
+    VStack{
+      HStack{
+        Image(systemName: "photo")
+          .onTapGesture {
+            isPresenting = true
+          }
+
+        Image(systemName: "camera")
+      }
+      .font(.largeTitle)
+      .foregroundColor(.blue)
+
+      Rectangle()
+        .strokeBorder()
+        .foregroundColor(.yellow)
+      .overlay(
+        Group {
+          if uiImage != nil {
+            Image(uiImage: uiImage!)
+              .resizable()
+              .scaledToFit()
+          }
+        }
+      )
     }
+    .sheet(isPresented: $isPresenting){
+      ImagePicker(uiImage: $uiImage, isPresenting: $isPresenting)
+    }
+
+    .padding()
+
+  }
+
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+
+  static var previews: some View {
+
+    ContentView()
+
+  }
+
 }
