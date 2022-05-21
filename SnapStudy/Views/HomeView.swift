14b9c4d9a481ac7c4c3c371ffdf03ef2edf99bf9
jomillsjoseanand <44730936+jomillsjoseanand@users.noreply.github.com>
2022-05-21 18:36:26 -0400

    Incorporate haptic feedback
    Added haptic vibration when the user
    taps the flashcard.

--- SnapStudy/Views/HomeView.swift
+++ SnapStudy/Views/HomeView.swift
@@ -6,19 +6,25 @@
 */
 
 import SwiftUI
+import CoreHaptics
 
 struct HomeView: View {
     
-    
     // State variables for dragging flashcard state
+    
     @State private var offset = CGSize.zero
     @State private var isDragging = false
+    
+    // Variable for device haptics
+    
+    @State private var engine: CHHapticEngine?
     
     
     var body: some View {
         
         // Variables for long pressing and dragging flashcards
         
+        // Drag specific gesture
         let dragGesture = DragGesture()
             .onChanged { value in
                 offset = value.translation
@@ -28,6 +34,7 @@ struct HomeView: View {
                 isDragging = false
             }
         
+        // Long press specific gesture
         let pressGesture = LongPressGesture()
             .onEnded { value in
                 withAnimation {
@@ -35,6 +42,7 @@ struct HomeView: View {
                 }
             }
         
+        // Drag gesture gets triggered only when long pressed
         let combined = pressGesture.sequenced(before: dragGesture)
         
         VStack{
@@ -46,7 +54,45 @@ struct HomeView: View {
             .scaleEffect(isDragging ? 1.5 : 1)
             .offset(offset)
             .gesture(combined)
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: tapHaptic)
     }
+    
+    
+    // Haptic feedback functions
+    
+    // Prepares haptics if supported, else returns
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error: \(error.localizedDescription)")
+        }
+    }
+    
+    // Tap haptic feedback
+    func tapHaptic() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play success haptic with error: \(error.localizedDescription)")
+        }
+    }
+    
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
