7ab9bc0dbaa3b672711c3b83a8f5301d7a12843b
jomillsjoseanand <44730936+jomillsjoseanand@users.noreply.github.com>
2022-05-21 17:55:05 -0400

    Add long press and drag gestures
    Implemented long press gesture for when
    the user holds the card. Added the drag
    feature so that the user can move the card
    to either side of the screen.
    
    Yet to implement the tap gesture to flip
    the flashcards.

--- SnapStudy/Views/HomeView.swift
+++ SnapStudy/Views/HomeView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    
+    // State variables for dragging flashcard state
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+    
+    
     var body: some View {
-        Text("Home Page")
+        
+        // Variables for long pressing and dragging flashcards
+        
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offset = value.translation
+            }
+            .onEnded { _ in
+                offset = .zero
+                isDragging = false
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        VStack{
+            Text("Home Page")
+                .onTapGesture {
+                    print("Text Tapped")
+                }
+        }
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
     }
 }
 
